daab9c553a9230f1f035085e754647bff49555d3
Taesub Yun <taesubyun@icloud.com>
2022-01-30 15:41:49 +0900

    구조 잡기

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,50 @@
 import styled from 'styled-components';
 
 const Navbar = () => {
-    return <div></div>;
+    return (
+        <nav style={{ backgroundColor: '#ececec', padding: '10px 25px' }}>
+            <ul
+                style={{
+                    listStyle: 'none',
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                }}
+            >
+                <li>
+                    <a
+                        style={{ color: 'teal', textDecoration: 'none' }}
+                        href='#'
+                    >
+                        HOME
+                    </a>
+                </li>
+                <li>
+                    <a
+                        style={{ color: 'teal', textDecoration: 'none' }}
+                        href='#'
+                    >
+                        HOME
+                    </a>
+                </li>
+                <li>
+                    <a
+                        style={{ color: 'teal', textDecoration: 'none' }}
+                        href='#'
+                    >
+                        HOME
+                    </a>
+                </li>
+                <li>
+                    <a
+                        style={{ color: 'teal', textDecoration: 'none' }}
+                        href='#'
+                    >
+                        HOME
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    );
 };
 
 export default Navbar;
