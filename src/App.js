dea414ca8d33c30d79281dea24a786dd6e9e7f8b
Taesub Yun <taesubyun@icloud.com>
2022-02-02 01:29:17 +0900

    Footer 컴포넌트로 수정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Section from './components/Section';
+import Footer from './components/Footer';
 
 function App() {
     return (
         <div className='App'>
             <Navbar />
             <Section />
+            <Footer />
         </div>
     );
 }
