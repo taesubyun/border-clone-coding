f99af588dc883eee08b6a9c6dff9a8abca179389
Taesub Yun <taesubyun@icloud.com>
2022-02-04 21:35:27 +0900

    Footer 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Section from './components/Section';
 import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <Navbar />
             <Section />
             <Main />
+            <Footer />
         </div>
     );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,10 @@
-const Footer = () => {
+const Main = () => {
     return (
         <div
             style={{
                 border: '1px solid #F7FD04',
                 padding: '5px',
                 margin: '15px 5px',
-                height: '50vh',
             }}
         >
             <div
@@ -13,13 +12,27 @@ const Footer = () => {
                     border: '1px solid #EE2B47',
                     width: '100%',
                     height: '100%',
-                    paddingLeft: '10px',
                 }}
             >
-                <h2 style={{ color: '#EE2B47' }}>M A I N</h2>
+                <h2 style={{ color: '#EE2B47', paddingLeft: '10px' }}>
+                    M A I N
+                </h2>
+                <div
+                    style={{
+                        border: '1px solid #EE2B47',
+                        margin: '8px',
+                    }}
+                >
+                    <div
+                        style={{
+                            width: '500px',
+                            height: '80px',
+                        }}
+                    ></div>
+                </div>
             </div>
         </div>
     );
 };
 
-export default Footer;
+export default Main;
